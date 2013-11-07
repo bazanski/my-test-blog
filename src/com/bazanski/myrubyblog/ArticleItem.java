787a59701647676a1a36b40d59630ba06506346a
bazanski <bazbany@gmail.com>
2013-11-07 09:53:49 +0400

    Added Getters and Setters to ArticleClass + Authors IDs

--- src/com/bazanski/myrubyblog/ArticleItem.java
+++ src/com/bazanski/myrubyblog/ArticleItem.java
@@ -1,16 +1,47 @@
 package com.bazanski.myrubyblog;
 
+import java.util.ArrayList;
+
 public class ArticleItem {
 	private String title;
 	private String text;
 	private String dateTime;
 	private String author;
+	private long authorID;
+	private ArrayList<CommentItem> comments;
 	
-	public ArticleItem(String title, String text, String dateTime, String author) {
+	public ArticleItem(String title, String text, String dateTime, String author, ArrayList<CommentItem> all_comments, long authorID) {
 		this.title = title;
 		this.text = text;
 		this.dateTime = dateTime;
 		this.author = author;
+		this.comments = all_comments;
+		this.authorID = authorID;
 	}
-
+	//============Setters==========
+	
+	//============Getters==========
+	public String getText() {
+		return this.text;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getDateTime() {
+		return this.dateTime;
+	}
+	
+	public String getAuthor() {
+		return this.author;
+	}
+	
+	public long getAuthorID() {
+		return this.authorID;
+	}
+	
+	public ArrayList<CommentItem> getComments() {
+		return this.comments;
+	}
 }
--- src/com/bazanski/myrubyblog/CommentItem.java
+++ src/com/bazanski/myrubyblog/CommentItem.java
@@ -4,12 +4,14 @@ public class CommentItem {
 	private String commentText;
 	private String commentAuthor;
 	private String creationDateTime;
+	private long commentAuthorID;
 	//статья к которой оставлен
 	
-	public CommentItem(String commentText, String commentAuthor, String creationDateTime) {
+	public CommentItem(String commentText, String commentAuthor, String creationDateTime, long commentAuthorID) {
 		this.commentText = commentText;
 		this.commentAuthor = commentAuthor;
 		this.creationDateTime = creationDateTime;
+		this.commentAuthorID = commentAuthorID;
 	}
 	
 }
